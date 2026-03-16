d11c6c444e087c684a474778b77aa4c97f187114
wyattbrashear <bfamily610@icloud.com>
2026-03-16 12:28:53 -0400

    Add the build function

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+    with open(f"{directory}.507ex", 'wb') as f:
+        f.write("FZX2".encode())
+        f.write("\n!507EX-METADATA".encode())
+        f.write(f"\n507ex-hash|{exec_hash}".encode())
+        f.write(f"\n507ex-hashmode|blake2s".encode())
+        f.write(f"\n507ex-id|{uuid.uuid4()}".encode())
+        #DTOC - Date/Time of Creation
+        f.write(f"\n507ex-dtoc|{datetime.now().now()}".encode())
+        f.write(f"\n507ex-depends|{depend_file}".encode())
+        f.write(f"\n!507EX-DEPENDENCIES\n{dependencies}".encode())
+        f.write("\n!507EX-END-META\n".encode())
+        f.write(exec_contents)
+    print(f"Successfully built {directory}.507ex")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
